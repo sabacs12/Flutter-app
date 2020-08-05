cc29fcedfa365f2fc8fb660f2159791d6ea334b5
sabacs12 <45117361+sabacs12@users.noreply.github.com>
2020-08-06 00:03:44 +0530

    Create main.dart

---
+++ task1/main.dart
@@ -0,0 +1,17 @@
+import 'home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: myApp(),
+      debugShowCheckedModeBanner: false,
+
+    );
+  }
+}
